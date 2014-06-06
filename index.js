8b26ea5b3815f4cf947c1198191c57af617acaf5
scottleo <wyuleo@gmail.com>
2014-06-06 18:07:29 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var template = require('art-template');
+
+module.exports = function(content, file, conf) {
+  fis.util.merge(template.defaults, conf);
+  
+  return template.compile(content).toString()
+    //.replace(/^function anonymous/, 'function')
+    .replace('$utils=this', '$utils=template.utils');
+};
