9c885d39c3ddf169fb0f65c9e61a126c062592e0
scottleo <wyuleo@gmail.com>
2014-06-06 19:17:48 +0800

    remove anonymous

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ module.exports = function(content, file, conf) {
   fis.util.merge(template.defaults, conf);
   
   return template.compile(content).toString()
-    //.replace(/^function anonymous/, 'function')
+    .replace(/^function anonymous/, 'function')
     .replace('$utils=this', '$utils=template.utils');
 };
